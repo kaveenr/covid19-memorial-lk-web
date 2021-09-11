1a96e43fcd90d58d16946278403894a94887f9d3
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2021-09-11 19:07:51 +0530

    bootstrap!

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={"md:container mx-auto px-4 py-1 h-screen"}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
