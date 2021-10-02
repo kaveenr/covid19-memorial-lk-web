41f7bdcece80b55f014d9b45283013f666317ff9
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2021-10-02 20:10:51 +0530

    transition fix.

--- components/Overlay.js
+++ components/Overlay.js
@@ -18,13 +18,13 @@ const Overlay = ({ data, close }) => {
         <>
             <div className={`fixed top-0 left-0 w-screen h-screen bg-black z-40 opacity-50 overflow-hidden ${!data ? "hidden" : "fixed"}`}/>
             {data ? (
-                <div className="card rounded-none bg-base-200 w-full lg:w-1/2 rounded-t-xl z-50 fixed bottom-0 left-0 lg:left-36 px-8 pb-6">
+                <div className={`card rounded-none bg-base-200 w-full lg:w-1/2 rounded-t-3xl z-50 fixed bottom-0 left-0 lg:left-36 px-8 pb-6`}>
                     <div className="absolute top-2 right-1">
                         <button className="px-4 py-2" onClick={()=> {close()}}>
                             <FontAwesomeIcon size="lg" icon={faTimes} />
                         </button>
                     </div>
-                    <div className={`flex flex-col items-center relative transition-transform duration-700 ease-out py-4`}>
+                    <div className={`flex flex-col items-center relative py-4`}>
                         <Image src={FlowerImg} width="150" height="150" loading="lazy" className="flex-grow w-full"/>
                         <p className="text-sm font-semibold mt-1">{data.attributes.ageValue}, {t(data.attributes.gender)}</p>
                         <p className="text-xs font-semibold">{t('place', {place: data.attributes.city})}</p>
