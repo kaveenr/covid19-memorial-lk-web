db81facebb6614e69e2bcf1494bfece00575baba
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2021-10-18 23:11:22 +0530

    added fetch import.

--- utils/captcha.js
+++ utils/captcha.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 async function validateCaptchaResponse(res) {
 
     const params = new URLSearchParams();
--- utils/zeptomail.js
+++ utils/zeptomail.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 export default class ZeptoClient {
 
     static FROM_ADDRESS = {
