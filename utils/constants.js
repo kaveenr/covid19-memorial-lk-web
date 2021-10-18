d22cb3398805a22d8c7e7f4709a1c6f525bd40c2
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2021-10-18 10:24:09 +0530

    reset constants.

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@ export const GA_TRACKING_ID = 'G-GEZ96EYPEQ';
 export const AUDIENCE_SUBMISSIONS = [
     {
         email_address: {
-            address: 'kaveen@ukr.lk',
+            address: 'submissions@srilankac19memorial.org',
             name: 'Sri Lanka C19 Memorial Submissions'
         }
     }
@@ -10,7 +10,7 @@ export const AUDIENCE_SUBMISSIONS = [
 export const AUDIENCE_INFO = [
     {
         email_address: {
-            address: 'kaveen@ukr.lk',
+            address: 'info@srilankac19memorial.org',
             name: 'Sri Lanka C19 Memorial Info'
         }
     }
